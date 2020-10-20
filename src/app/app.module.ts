26f260ffe228717d400ea5619ef3b51cfb892e78
Manuel <manuel.szecsenyi@gmail.com>
2020-10-20 15:16:38 +0200

    finished v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ImprintComponent } from './imprint/imprint.component';
-import { SwiperModule } from 'ngx-swiper-wrapper';
 
+import { SwiperModule, SwiperConfigInterface, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+  direction: 'horizontal',
+  threshold: 50,
+  spaceBetween: 5,
+  slidesPerView: 1,
+  centeredSlides: true
+};
 
 @NgModule({
   declarations: [
@@ -19,7 +28,12 @@ import { SwiperModule } from 'ngx-swiper-wrapper';
     AppRoutingModule,
     SwiperModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SwiperComponent, SwiperDirective } from 'ngx-swiper-wrapper';
+import { SwiperOptions } from 'swiper';
+import { PaginationOptions } from 'swiper/types/components/pagination';
+import { ScrollbarOptions } from 'swiper/types/components/scrollbar';
+
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImprintComponent implements OnInit {
 
+
+  public config: SwiperOptions = {
+    a11y: { enabled: true },
+    direction: 'vertical',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: false
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
