01d522aa3bfbb36b98e4c6d09874f3b4dbf585ad
Manuel <manuel.szecsenyi@gmail.com>
2020-12-03 09:06:38 +0100

    finished redesign

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import { SwiperComponent, SwiperDirective } from 'ngx-swiper-wrapper';
-import { SwiperOptions } from 'swiper';
-import { PaginationOptions } from 'swiper/types/components/pagination';
-import { ScrollbarOptions } from 'swiper/types/components/scrollbar';
+import axios from 'axios';
 
 @Component({
   selector: 'app-imprint',
@@ -13,20 +9,9 @@ import { ScrollbarOptions } from 'swiper/types/components/scrollbar';
 export class ImprintComponent implements OnInit {
 
 
-  public config: SwiperOptions = {
-    a11y: { enabled: true },
-    direction: 'vertical',
-    slidesPerView: 1,
-    keyboard: true,
-    mousewheel: true,
-    scrollbar: false,
-    navigation: true,
-    pagination: false
-  };
-
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
   }
 
 }
